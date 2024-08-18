e47d9c3f60cac2a242fcca9df59c2090a1665570
Fadliabdill <fadliabdill287@gmail.com>
2024-08-18 14:37:04 +0700

    translate

--- js/translate.js
+++ js/translate.js
@@ -2,29 +2,39 @@ document.getElementById("translateBtn").addEventListener("click", function () {
   document.getElementById("translateBtn").style.display = "none";
   document.getElementById("refreshBtn").style.display = "block";
 
-  document.getElementById("tanggal").textContent = "How to order?";
+  document.getElementById("tanggal").textContent = "Saturday, 24-08-2024";
   document.getElementById("tanggal2").textContent = "Saturday, 24-08-2024";
-  document.getElementById("hari2").textContent = "Day";
-  document.getElementById("jam2").textContent = "Hour";
-  document.getElementById("menit2").textContent = "Minute";
-  document.getElementById("detik2").textContent = "Second";
-  document.getElementById("online-shop").textContent = "Online Shop";
-  document.getElementById("top-seller").textContent = "Top Seller";
-  document.getElementById("des-prod-1").textContent = `This set is made from fine and soft denim, the material is cool, comfortable to wear and
-  It's not exposed so it's comfortable to wear. With long sleeves and full buttons. Suitable for use
-  in various formal and casual events`;
-  document.getElementById("des-prod-2").textContent = `This set is made from fine and soft denim, the material is cool, comfortable to wear and
-  It's not exposed so it's comfortable to wear. With long sleeves and full buttons. Suitable for use
-  in various formal and casual events`;
+  document.getElementById("alerts").textContent = "Calling all Fashion fans";
+  document.getElementById("alerts-des").textContent = "Calling all Fashion who wants to Fashion update, Please come and join JI Premium at our booth FNPbyFlorina Sell Up to 50%, FNPbyFlorina to update your style See you there!";
+  document.getElementById("discount").textContent = "Get Discount";
+  document.getElementById("des-shop").textContent = "You can order through our Online Shop or through our Official Website https://vnpbyflorina.com";
+  document.getElementById("ondiscount").textContent = "Currently on Discount";
+  document.getElementById("ondiscount-des").textContent = "Only at the Jakarta Convention Center Exhibition can you get up to 50% discount.";
+  document.getElementById("Guaranteed").textContent = "Satisfaction Guaranteed";
+  document.getElementById("Guaranteed-des").textContent = "Customer satisfaction is our top priority. We are ready to serve you wholeheartedly.";
+  document.getElementById("shopping").textContent = "Shop Through Our Website";
+  document.getElementById("shopping-t").textContent = "Scan the QR code below to order via our website";
+  document.getElementById("shopping-des").textContent = "We offer a wide range of quality products designed to meet your needs and desires. Discover our collection of clothing, accessories, and other fashion items that will elevate your style. With an easy ordering system, you can shop comfortably from home. Enjoy a safe and fast shopping experience, and take advantage of exciting offers every day. We are committed to providing the best service to ensure your satisfaction with every purchase. So, don't hesitate to explore our catalog and find your favorite items!";
   document.getElementById("lokasi").textContent = "Location";
   document.getElementById("btn-gmap").textContent = "Open Google Maps";
-  document.getElementById("confirm").textContent = "confirm your presence";
-  document.getElementById("diskon").textContent = "Please confirm your presence to get a discount voucher for shopping at V&P by Florina.";
-  document.getElementById("btn-diskon").textContent = "Get Discount!!";
-  document.getElementById("hadir").textContent = "Present";
-  document.getElementById("tidak").textContent = "not present";
-  document.getElementById("send").textContent = "Send";
-  document.getElementById("end").textContent = "We are looking forward to your presence";
+  document.getElementById("confirm").textContent = "Confirm your attendance";
+  document.getElementById("diskon").textContent = "Please confirm your attendance to receive a shopping discount voucher at V&P by Florina.";
+  document.getElementById("btn-diskon").textContent = "Get Discount";
+  document.getElementById("voucher").textContent = "How to Use the Voucher";
+  document.getElementById("voucher-des").textContent = "Claim and download your virtual discount through this invitation.";
+  document.getElementById("voucher-des2").textContent = "Visit the V&P by Florina booth available at JIPREMIUM JCC 2024.";
+  document.getElementById("voucher-des2-1").textContent = "Visit the V&P by Florina booth available at JIPREMIUM JCC 2024.";
+  document.getElementById("voucher-des3").textContent = "Select the items you want to purchase.";
+  document.getElementById("voucher-des4").textContent = "Show the Virtual Voucher to the V&P by Florina staff.";
+  document.getElementById("voucher-des4-1").textContent = "Show the Virtual Voucher to the V&P by Florina staff.";
+
+  document.getElementById("jam2").textContent = "Hour";
+  document.getElementById("hari2").textContent = "Day";
+  document.getElementById("menit2").textContent = "Minute";
+  document.getElementById("detik2").textContent = "Second";
+  document.getElementById("des-home").textContent = "A fashion exhibition that will change the way you view style.";
+  document.getElementById("when").textContent = "will take place on";
+  document.getElementById("invite").textContent = "We invite you";
 });
 
 document.getElementById("refreshBtn").addEventListener("click", function () {
